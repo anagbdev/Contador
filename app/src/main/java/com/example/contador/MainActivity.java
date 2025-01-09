763f5b82312cb658e2bd9483ff64ed3bffb81c85
Anabel Guzmán Bandera <anaguzmanb97@gmail.com>
2025-01-09 18:13:21 +0100

    Fix functionality issues

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -7,10 +7,11 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Switch;
+import 	android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int counter =0;
+    public int counter = 0;
 
     TextView countText;
 
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TextView countText =(TextView)findViewById(R.id.counter);
+        TextView countText = (TextView) findViewById(R.id.counter);
     }
 
     //Method used to manage the orientation change
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Method used to increase the counter
 
-    public void increaseCounter(View View){
+    public void increaseCounter(View View) {
 
         counter++;
         showCount();
@@ -38,37 +39,49 @@ public class MainActivity extends AppCompatActivity {
 
     //Method used to decrease the counter
 
-    public void decreaseCounter(View View){
+    public void decreaseCounter(View View) {
 
         counter--;
 
-        Switch switchControl = (Switch)findViewById(R.id.allowNeg);
+        Switch switchControl = (Switch) findViewById(R.id.allowNeg);
 
         boolean allowNegative = switchControl.isChecked();
 
-        if(allowNegative || counter >0){
+        if (allowNegative || counter > 0) {
 
             counter--;
-            showCount();
 
+        } else {
+
+            counter = 0;
         }
+
+        showCount();
     }
 
     //Method used to restart the counter
 
-    public void resetCounter(View View){
+    public void resetCounter(View View) {
 
-        counter=0;
-        showCount();
+        EditText resetNumber = (EditText) findViewById(R.id.resetNumberInput);
 
+        if (resetNumber.getText().toString().isEmpty()) {
+            counter = 0;
+        } else {
+            counter = Integer.parseInt(resetNumber.getText().toString());
+            resetNumber.setText("");
+
+            showCount();
+
+        }
     }
+        // Method used to show the count
 
-    // Method used to show the count
+    public void showCount() {
 
-    public void showCount(){
+            TextView countText = (TextView) findViewById(R.id.counter);
 
-        TextView countText =(TextView)findViewById(R.id.counter);
+            countText.setText(Integer.toString(counter));
 
-        countText.setText(Integer.toString(counter));
     }
 }
